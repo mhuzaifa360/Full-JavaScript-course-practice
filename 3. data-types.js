2722402fef86d08aa720d854dd2fcb051855da04
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-25 21:54:16 +0500

    add practice qusetion file of js

---
+++ 3. data-types.js
@@ -0,0 +1,19 @@
+//? data types in js
+// number
+// string
+// boolean
+// indefined
+// null 
+// bigint
+// symbol
+
+// let age = 23;
+// let name = 'junaid';
+// let istrue = true;
+// let a ;
+// let varr = null
+// console.log(typeof(age));
+// console.log(typeof(name));
+// console.log(typeof(istrue));
+// console.log(typeof(a));
+// console.log(typeof(varr));
---
+++ 4. practice-question.js
@@ -0,0 +1,15 @@
+// practice quesiton till variables and data type
+
+var name = 'Parker Jotter Standard CT Ball Pen (Black)'
+let rating = 70002
+let offer = 5
+let price = 270
+isDeal = true
+
+console.log(name);
+console.log(rating);
+console.log(offer);
+console.log(isDeal);
+
+
+
