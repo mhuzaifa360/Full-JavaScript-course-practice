f1beb3e7918e38ed4d6c2280316edf31bd404429
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-25 20:29:08 +0500

    add only file which name is console.log in js

---
+++ 1. console.js
@@ -0,0 +1,5 @@
+// this page contain console.log in js
+// anything we want to print in console windows we have to use console.log 
+
+console.log('my name is huzaifa');
+console.log("huzaifa khan")
