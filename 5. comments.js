7ff1aa4af918da53bab7d0f7cb9a590c550a148e
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-26 00:02:15 +0500

    add operators file of js

---
+++ 5. comments.js
@@ -0,0 +1,5 @@
+// this is single line comment in js
+
+/* this is multiple line comment in js
+comment is used for write code which code edithor cannot run
+*/
---
+++ 6. operators.js
@@ -0,0 +1,39 @@
+//? operators in js
+
+//? 1. arithematics operators
+// let a = 4;
+// let b = 5;
+// console.log('addition : '+ (a+b));
+// console.log('subtraction : '+ (b-a));
+// console.log('multiplication :' +(a*b));
+// console.log('division : '+(a/b));
+// console.log('modulus : '+(b%a));
+// console.log('exponentiation : '+ (a**b))
+// console.log('increment : ' +  (++a))
+// console.log('decrement : '+ (--a))
+
+//? 2. assignment operators
+// =  +=  -=  *=  %=  **=
+// a += 1 means a = a + 1;
+// a -= 2 means a = a - 2;
+
+// let a = 5;
+// let b = 3;
+
+// a = b;
+// console.log(a);
+
+// a += 3;
+// console.log(a); // a = a + 3;
+
+// a -= 2;
+// console.log(a)
+
+// a *= 2;
+// console.log(a)
+
+// a %= 2;
+// console.log(a)
+
+// a **= 2;
+// console.log(a)
