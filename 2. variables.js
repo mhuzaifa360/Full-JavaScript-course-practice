a60fa04d6077f4945277a6ba29543f341149c09d
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-25 21:05:08 +0500

    add file contain variables in js

---
+++ 2. variables.js
@@ -0,0 +1,35 @@
+//? variables in js 
+//? rules of variables in js
+// variable names are case sensitive 
+// a and A is different in js
+// only letter, underscore(_) or $ should be 1st character 
+// reserved words cannot be variable name
+ 
+
+// name = 'huzaifa';
+// roll = 122;
+// city = 'peshawar';
+// isCheck = true;
+// console.log(
+//     'my name is '+name+' and my roll no is '+roll+' i am lives in '+city
+// );
+
+
+
+// var name = 'ali'
+// let roll = 12
+// const city = 'swabi'
+// console.log(
+//  'my name is '+name+' and my roll is '+roll+' i lives in city '+city
+// );
+
+
+//? variable scoping
+// var = variable can ve re-declared and updated. a global scop variable.
+
+// let = varibale cannot be re-declared but can be updated. a block scope variable
+
+// const = vairable cannot be re-declared or updated . a block scope variable. 
+
+
+
