d2bec45e61ed22f775a1210ee25cc08e8df01d40
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-26 20:03:30 +0500

    add conditional statement file of js

---
+++ 7. conditional-statement.js
@@ -0,0 +1,40 @@
+//? conditional statement
+//? if statement
+//? if else statement
+
+//! example 01
+// if a person age is equal to 20 or greater than 20 so he/she can vote other cannot allowed 
+
+// let age = 17
+// if (age >= 18){
+//     console.log('you can vote ...!');
+    
+// }else{
+//     console.log('your age is less than 18.');
+    
+// }
+
+//! example 02
+// if mode of screen is light than set to dark and also vise versa
+
+// let mode = 'light';
+// let color;
+
+// if(mode === 'dark'){
+//     color = 'black';
+// }else if(mode === 'light'){
+//     color = 'white';
+// }
+// console.log('your screen color is now : ',color);
+
+
+//! example 03
+// if a number is divide by 2 and remender is 0 than this number is even but if remander is 1 than this number is odd
+
+// let number = prompt('enter a number to check..')
+// if(number%2 === 0){
+//     console.log('this number is even');
+// }else{
+//     console.log('the number is odd');
+    
+// }
