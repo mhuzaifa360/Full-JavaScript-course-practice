dc3e781af947e44f7181b470ebe2ad7b3e3f39ba
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-01-26 15:45:03 +0500

    add oeprators file of js which contain all operation

--- 3. data-types.js
+++ 3. data-types.js
@@ -12,8 +12,17 @@
 // let istrue = true;
 // let a ;
 // let varr = null
+// let g = BigInt(1234)
+// let h = Symbol(hello)
 // console.log(typeof(age));
 // console.log(typeof(name));
 // console.log(typeof(istrue));
 // console.log(typeof(a));
 // console.log(typeof(varr));
+// console.log('the value of g : '+g);
+// console.log(typeof(g));
+// console.log('the value of h : '+h);
+// console.log(typeof(h));
+
+
+
--- 6. operators.js
+++ 6. operators.js
@@ -37,3 +37,66 @@
 
 // a **= 2;
 // console.log(a)
+
+
+//? Comparison operator
+// == equal to 
+// != non equal to 
+// === equal to and type
+// !== not equal to and type
+
+// let n1 = 1;
+// let n2 = 2;
+// console.log('is 1 is equal to : '+ (n1 == n2));
+// console.log('is 1 is not equal to 2 : '+ (n1 != n2));
+// console.log('are 1 is equal to string(1) : '+ (n1 == '1'));  // true
+// console.log('is 1 is equal to string(1) strickly : '+ (n1 === '1'));  // false
+
+
+// other comparison operators 
+// >  >=  <  <=
+
+// let n = 12;
+// let m = 21;
+// console.log('is n is greater than m : '+ (n > m)); // false
+// console.log('is n is less than m : '+ (n < m)); // true
+// console.log('is n is greater or equal to m : '+ (n >= m)); // false
+
+
+//? Logical oeprators
+// && and 
+// || or
+// !! not
+
+// let a = 1;
+// let b = 2;
+// let cond1 = a > b; // false
+// let cond2 = a === 1; // true
+
+//! && and
+// console.log('cond1 && cond2 : ', cond1 && cond2); //
+// console.log('true && true : ', true && true); // true
+// console.log('false && false : ', false && false); // false
+// console.log('true && false : ', true && false); // false
+// console.log('false && true : ', false && true); // false
+// console.log('');
+
+
+//! || or
+// console.log('true || true : ', true || true); // true
+// console.log('false || false : ', false || false); // false
+// console.log('true || false : ', true || false); // false
+// console.log('false || true : ', false || true); // false
+// console.log('');
+
+
+//! !! not
+// logical not convert true into false and false into true 
+ 
+// console.log('not true results : ', !true); // true
+// console.log('not false results : ', !false);
+
+
+
+ 
+
